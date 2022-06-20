4c362ca8676b3e547720b93fb2f7ba6fbfb276c4
lishuangling <595237147@qq.com>
2022-06-21 00:30:49 +0800

    作业

--- 03.js_basics/02_js的DOM/DOM练习/5.DOM事件/carousel.js
+++ 03.js_basics/02_js的DOM/DOM练习/5.DOM事件/carousel.js
@@ -10,26 +10,99 @@ container.appendChild(clone)
 
 let timer = setInterval(() => {
     move()
-}, 60);
+}, 10);
 let x = 0;
 function move() {
-    x -= 5
-    console.log(x);
+    x -= 1
     if (x == -720) {
-        x=0
+        x = 0
     }
-    container.style.transform =`translateX(${x}px)` 
+    container.style.transform = `translateX(${x}px)`
 }
-container.addEventListener('mouseenter',(e) => {
+container.addEventListener('mouseenter', (e) => {
     clearInterval(timer)
 })
-container.addEventListener('mouseleave',() => {
+container.addEventListener('mouseleave', () => {
     timer = setInterval(() => {
         move()
-    }, 60);
+    }, 10);
 })
 // 第二题
-let substance=document.querySelector('.substance')
+let substance = document.querySelector('.substance')
+let right = document.querySelector('.right')
+let left = document.querySelector('.left')
+let images = document.querySelector('.images')
+let h = 0, time, lock = 0;
 
+
+left.addEventListener('click', function () {
+    if (lock) return
+    lock = 1
+    // time = setInterval(() => {
+        transformLeft()
+        // clearInterval(time)
+    // }, 100);
+
+    setTimeout(() => {
+        lock = 0
+    }, 1000);
+
+})
+right.addEventListener('click', function () {
+    if (lock) return
+    lock = 1
+    // time = setInterval(() => {
+        transformRight()
+        // clearInterval(time)
+    // }, 100);
+    setTimeout(() => {
+        lock = 0
+    }, 1000);
+})
+function transformRight() {
+    h -= 400
+    images.style.transition = `1s`
+    images.style.transform = `translateX(${h}px)`
+    if (h == -2000) {
+        setTimeout(() => {
+            h = 0
+            images.style.transition = `0s`
+            images.style.transform = `translateX(${h}px)`
+        }, 1000);
+    }
+}
+function transformLeft() {
+    if (h == 0) {
+        h = -2000
+        images.style.transition = `0s`
+        images.style.transform = `translateX(${h}px)`
+        setTimeout(() => {
+            h += 400
+            images.style.transition = `1s`
+            images.style.transform = `translateX(${h}px)`
+        }, 1);
+        return
+    }
+    h += 400
+    images.style.transition = `1s`
+    images.style.transform = `translateX(${h}px)`
+}
+
+let minute=document.querySelector('.minute')
+let second=document.querySelector('.second')
+let hour=document.querySelector('.hour')
+let s=0,ho=0,m=0;
+setInterval(() => {
+    s+=6
+    second.style.transform= `rotate(${s}deg)`;
+}, 1000);
+setInterval(() => {
+    m+=6
+    minute.style.transform= `rotate(${m}deg)`;
+}, 1000*60);
+setInterval(() => {
+    ho+=1
+    hour.style.transform= `rotate(${ho}deg)`;
+}, 1000*60*10);
 
 
