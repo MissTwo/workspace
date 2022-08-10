15b96445f3d6973b7fa85af86073df4da6e057ea
lishuangling <595237147@qq.com>
2022-08-10 11:58:00 +0800

    11

--- webpack/02.webpack_demo/config/webpack.base.config.js
+++ webpack/02.webpack_demo/config/webpack.base.config.js
@@ -1,17 +1,25 @@
-const path=require('path');
+const path = require('path');
 // html页面的创建
-const HtmlWebpackPlugin=require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+// css文件分离
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// css文件压缩
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+// 问题：压缩了css会导致js无法进行压缩，故需要单独配置压缩js文件
+// js文件压缩
+const TerserPlugin = require('terser-webpack-plugin');
+
 // 服务器的配置
 // const Webpack = require('webpack');
 // const WebpackDevServer = require('webpack-dev-server');
 module.exports = {
     entry: {
-        index:'./src/js/index.js'
+        index: './src/js/index.js'
     },
-    output:{
+    output: {
         path: path.resolve(__dirname, '../dist'),
         filename: '[name].[hash].bundle.js',
-        clean: true,
+        clean: true
     },
     mode: 'development',
     plugins: [
@@ -22,15 +30,17 @@ module.exports = {
             // 文件名字
             filename: 'index.html',
             // 注入js的位置,默认是head
-            inject:"body",
+            inject: "body",
             // 指定html引入的模块，模块名称就是entry入口中的key
-            chunks:["index"]
+            chunks: ["index"]
         }),
+        // 引入css文件分离的插件对象
+        new MiniCssExtractPlugin()
 
     ],
-    devServer:{
-        static:"../dist",
-        port:8080,
+    devServer: {
+        static: "../dist",
+        port: 8080,
         // 'local-ip' | 'local-ipv4' | 'local-ipv6'，只支持局域网ip地址访问
         // localhost, 127.0.0.1
         // 0.0.0.0：支持任何地址访问
@@ -47,8 +57,34 @@ module.exports = {
         //     logging: 'info',
         //     overlay:true
         // },
-        // hot: true, // 热替换
+        hot: true, // 热替换
         // liveReload: true, // 热加载
+        proxy:{
+            '/api':{
+                target: 'http://localhost:3000',
+                pathRewrite:{'/api':''}
+            }
+        }
+    },
+    module: {
+        rules: [
+            // css文件分离处理与css样式分离会出现重复报错问题
+            {
+                // css样式分离
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader']
+            }
 
+        ]
+    },
+    // 优化
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new CssMinimizerPlugin(),
+            new TerserPlugin()
+        ]
     }
-}
+
+
+};
--- webpack/02.webpack_demo/src/js/index.js
+++ webpack/02.webpack_demo/src/js/index.js
@@ -1,4 +1,9 @@
 import $ from 'jquery';
-$('body').append("<button type='button'>点我打印<button/>").click(() => {
-    $('body').append("<div>我是一个新盒子</div>")
-})
+import '../css/index.css';
+
+$('body').append(`<input style='width: 200px; display: block' type="text">`);
+$('body').append(`<button type="button" style='width: 100px;'>点我创建盒子</button>`);
+$('input[type="text"]').val('12345');
+$('button[type="button"]').click(() => {
+    $('body').append("<div class='d1'></div>");
+});
